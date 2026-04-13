f99c9f81dee41ce242c76d6470ef119343483f3c
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-13 10:00:54 +0530

    implemented sorting

--- src/main/java/com/HexxaStores/service/CategoryService.java
+++ src/main/java/com/HexxaStores/service/CategoryService.java
@@ -4,7 +4,7 @@ import com.HexxaStores.payload.CategoryDTO;
 import com.HexxaStores.payload.CategoryResponse;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(Integer pageNumber , Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber , Integer pageSize , String sortBy , String sortOrder);
     CategoryDTO CreateCategories(CategoryDTO categoryDTO);
 
     CategoryDTO deleteCategory(Long categoryId);
